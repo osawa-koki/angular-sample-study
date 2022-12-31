2d136b2b95f624ecca11a530b8c584fb461ab00a
osawa-koki <github@osawa.dev>
2023-01-01 03:16:29 +0900

    ルーティング追加した。

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { AjaxComponent } from './ajax/ajax.component';
 import { FormComponent } from './form/form.component';
 import { IndexComponent } from './index/index.component';
+import { ModelComponent } from './model/model.component';
 
 const routes: Routes = [
   // ルーティングの設定
   { path: '', component: IndexComponent },
   { path: 'about', component: AboutComponent },
   { path: 'form', component: FormComponent },
+  { path: 'model', component: ModelComponent },
+  { path: 'ajax', component: AjaxComponent },
 ];
 
 @NgModule({
